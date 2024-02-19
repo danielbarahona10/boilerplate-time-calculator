68668d1d345e82b64ba6f80bd38623902b216ba1
danielbarahona10 <79060991+danielbarahona10@users.noreply.github.com>
2024-02-19 16:10:55 +0000

    Basic addition of time.
    Basic addition of time. Only considers the modulo 60 for minutes, and modulo 12 for hours. Additionally, it considers the format of one digit numbers to two digit numbers.

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,16 @@
-def add_time(start, duration):
+def add_time(start, duration, start_day = "Monday"):
+  new_time = ""
+  start_time = start.split()
+  start_hour = int(start_time[0].split(":")[0])
+  start_minute = int(start_time[0].split(":")[1])
+  am_pm = start_time[1]
+  
+  duration_hour = int(duration.split(":")[0])
+  duration_minute = int(duration.split(":")[1])
 
+  new_minute = (start_minute + duration_minute)%60
+  new_hour = (start_hour + duration_hour + (start_minute + duration_minute)//60)%12
 
-
-
-
-    return new_time
+  new_time = f'{new_hour:02d}' + ":" + f'{new_minute:02d}' + " " + am_pm
+  
+  return new_time
