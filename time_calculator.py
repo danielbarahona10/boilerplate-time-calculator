228be337e5d12e7443df4794102df4a1f540b6aa
danielbarahona10 <79060991+danielbarahona10@users.noreply.github.com>
2024-02-20 02:11:44 +0000

    Next day
    If the result will be the next day, it shows (next day) after the time. If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later.

--- time_calculator.py
+++ time_calculator.py
@@ -10,16 +10,33 @@ def add_time(start, duration, start_day = "Monday"):
   # AM/PM of the start time
   am_pm = start_time[1]
 
+  # Absolute minutes of the start time
+  if am_pm == "AM":
+    start_abs_minutes = start_hour * 60 + start_minute
+  else:
+    start_abs_minutes = (start_hour+12) * 60 + start_minute
+  
   # Hours of the duration
   duration_hour = int(duration.split(":")[0])
   # Minutes of the duration
   duration_minute = int(duration.split(":")[1])
 
+  # Absolute minutes of the duration
+  duration_abs_minutes = duration_hour * 60 + duration_minute
+  
+  # Number of days later
+  days_later = (start_abs_minutes + duration_abs_minutes) // (24*60)
+  
   # Calculate the new time
   new_minute = (start_minute + duration_minute)%60
   new_hour = (start_hour + duration_hour + (start_minute + duration_minute)//60)%12
 
   # Format the new time
-  new_time = f'{new_hour:02d}' + ":" + f'{new_minute:02d}' + " " + am_pm
+  if days_later == 0:
+    new_time = f'{new_hour:02d}' + ":" + f'{new_minute:02d}' + " " + am_pm
+  elif days_later == 1:
+    new_time = f'{new_hour:02d}' + ":" + f'{new_minute:02d}' + " " + am_pm + " (next day)"
+  else:
+    new_time = f'{new_hour:02d}' + ":" + f'{new_minute:02d}' + " " + am_pm + " (" + str(days_later) + " days later)"
   
   return new_time
